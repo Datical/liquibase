4481a1b8f13106ce6e69f57f35a8d9a81b05b980
Wesley Willard <wwillard@datical.com>
2019-07-31 15:14:06 -0500

    DAT-2702 Make LicenseServiceUtils.checkForValidLicense method just return empty list

--- liquibase-core/src/main/java/liquibase/license/LicenseServiceUtils.java
+++ liquibase-core/src/main/java/liquibase/license/LicenseServiceUtils.java
@@ -14,22 +14,7 @@ public class LicenseServiceUtils {
     private static Logger LOG = LogFactory.getLogger();
 
     public static ValidationErrors checkForValidLicense(String licenseType, ChangeSet changeSet) {
-      LicenseService licenseService = LicenseServiceFactory.getInstance().getLicenseService(licenseType);
-      if (licenseService == null) {
-        return new ValidationErrors();
-      }
-
-      if (licenseService.licenseIsValid(licenseType)) {
-        String message = "Found valid LiquibasePro license";
-        LOG.info(message);
-        return new ValidationErrors();
-      }
       ValidationErrors validationErrors = new ValidationErrors();
-      String message = "Change Set ID: " + changeSet.getId() + " Change Set Author: " + changeSet.getAuthor() + "\n";
-      message += "Change Type 'pro:createFunction' is not allowed without a valid Liquibase Pro License.\n";
-      message += "To purchase or renew a Liquibase Pro license key please contact lbprosupport@datical.com or\n" +
-                "go to https://download.liquibase.org/download/pricing";
-      validationErrors.addError(message);
       return validationErrors;
     }
 }
