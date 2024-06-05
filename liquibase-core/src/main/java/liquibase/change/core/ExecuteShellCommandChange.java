662ef564c0e096301fbad1d6a2aa45d45057f0ee
atytarchuk <atytarchuk@liquibase.com>
2024-06-05 13:46:48 +0200

    [DAT-17735] Process output even when process was killed by us to give customer more info about error output

--- liquibase-core/src/main/java/liquibase/change/core/ExecuteShellCommandChange.java
+++ liquibase-core/src/main/java/liquibase/change/core/ExecuteShellCommandChange.java
@@ -47,6 +47,7 @@ public class ExecuteShellCommandChange extends AbstractChange {
     private static final Long SECS_IN_MILLIS = 1000L;
     private static final Long MIN_IN_MILLIS = SECS_IN_MILLIS * 60;
     private static final Long HOUR_IN_MILLIS = MIN_IN_MILLIS * 60;
+    private static final int KILLED_PROCESS_RETURN_CODE = 143;  // return code when we kill process after timeout
 
     protected Integer maxStreamGobblerOutput = null;
 
@@ -265,7 +266,8 @@ public class ExecuteShellCommandChange extends AbstractChange {
                 // check if we timed out or not
                 if (timedOut.get()) {
                     String timeoutStr = timeout != null ? timeout : timeoutInMillis + " ms";
-                    throw new TimeoutException("Process timed out (" + timeoutStr + ")");
+                    LogFactory.getInstance().getLog().severe("Process timed out (" + timeoutStr + ")");
+                    return KILLED_PROCESS_RETURN_CODE;
                 }
             } catch (InterruptedException ignore) {
                 // check again
