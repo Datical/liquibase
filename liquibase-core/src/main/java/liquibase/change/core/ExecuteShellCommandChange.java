6fee33ae8a4d4d31a4f8387919a27017c8e3b2eb
atytarchuk <atytarchuk@liquibase.com>
2024-06-06 15:13:37 +0200

    [DAT-17735] Formatting commit

--- liquibase-core/src/main/java/liquibase/change/core/ExecuteShellCommandChange.java
+++ liquibase-core/src/main/java/liquibase/change/core/ExecuteShellCommandChange.java
@@ -276,7 +276,7 @@ public class ExecuteShellCommandChange extends AbstractChange {
             }
         }
 
-            return processExitCode;
+        return processExitCode;
     }
 
     /**
